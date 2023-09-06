f03217352239f2136643215284bdf565f4b1b373
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-09-06 16:54:40 +0200

    add Eventlistener prev and next

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -9,6 +11,36 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 41;
+let page = 1;
+let searchQuery = "";
+
+async function fetchCharacters() {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`
+  );
+  const data = await response.json();
+  cardContainer.innerHTML = "";
+  data.results.forEach((character) => {
+    const newCharacterCard = createCharacterCard(character);
+    cardContainer.append(newCharacterCard);
+  });
+}
+fetchCharacters();
+
+prevButton.addEventListener("click", () => {
+  console.log("prev-button clicked", page);
+  if (page === 1) {
+    return;
+  }
+  page--;
+  fetchCharacters();
+});
+nextButton.addEventListener("click", () => {
+  console.log("next-button clicked", page);
+  if (page === maxPage) {
+    return;
+  }
+  page++;
+  fetchCharacters();
+});
